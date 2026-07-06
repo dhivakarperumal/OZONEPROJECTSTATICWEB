89287dd0aa514c6d88740c1cccee846c89f88073
qtechx001 <qtechx001@gmail.com>
2026-07-06 15:43:55 +0530

    Images added

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,15 +1,15 @@
 export const galleryItems = [
   // Windows
-  { id: 1, image: "../Gallery/windows1.jpg", title: "Sliding Window System", category: "Windows", span: "col-span-2 row-span-2" },
-  { id: 2, image: "../Gallery/windows2.jpg", title: "Casement Fitment", category: "Windows", span: "" },
-  { id: 3, image: "../Gallery/windows3.jpg", title: "Bay Window Installation", category: "Windows", span: "" },
-  { id: 4, image: "../Gallery/windows4.jpg", title: "Aluminum Frame Windows", category: "Windows", span: "" },
+  { id: 1, image: "../Gallery/mosquitonet1.jpg", title: "Sliding Window System", category: "Windows", span: "col-span-2 row-span-2" },
+  { id: 2, image: "../Gallery/mosquitonet2.jpg", title: "Casement Fitment", category: "Windows", span: "" },
+  { id: 3, image: "../Gallery/mosquitonet3.jpeg", title: "Bay Window Installation", category: "Windows", span: "" },
+  { id: 4, image: "../Gallery/mosquitonet4.jpeg", title: "Aluminum Frame Windows", category: "Windows", span: "" },
   
   // Railings
-  { id: 5, image: "../Gallery/railings1.jpg", title: "Glass Balcony Railing", category: "Railings", span: "col-span-2 row-span-2" },
-  { id: 6, image: "../Gallery/railings2.jpg", title: "Stainless Steel Railing", category: "Railings", span: "" },
-  { id: 7, image: "../Gallery/railings3.jpg", title: "Decorative Railing Design", category: "Railings", span: "" },
-  { id: 8, image: "../Gallery/railings4.jpg", title: "Modern Glass Railing", category: "Railings", span: "" },
+  { id: 5, image: "../Gallery/railing1.jpg", title: "Glass Balcony Railing", category: "Railings", span: "col-span-2 row-span-2" },
+  { id: 6, image: "../Gallery/railing2.jpg", title: "Stainless Steel Railing", category: "Railings", span: "" },
+  { id: 7, image: "../Gallery/railing3.jpg", title: "Decorative Railing Design", category: "Railings", span: "" },
+  { id: 8, image: "../Gallery/railing4.jpg", title: "Modern Glass Railing", category: "Railings", span: "" },
   
   // Doors
   { id: 9, image: "../Gallery/doors1.jpg", title: "Folding Door System", category: "Doors", span: "col-span-2 row-span-2" },
@@ -18,10 +18,10 @@ export const galleryItems = [
   { id: 12, image: "../Gallery/doors4.jpg", title: "Glass Door Partition", category: "Doors", span: "" },
   
   // Shutters
-  { id: 13, image: "../Gallery/shutters1.jpg", title: "Louvre Shutters", category: "Shutters", span: "col-span-2 row-span-2" },
-  { id: 14, image: "../Gallery/shutters2.jpg", title: "Motorized Shutters", category: "Shutters", span: "" },
-  { id: 15, image: "../Gallery/shutters3.jpg", title: "Vertical Blinds", category: "Shutters", span: "" },
-  { id: 16, image: "../Gallery/shutters4.jpg", title: "Roller Shutters", category: "Shutters", span: "" },
+  { id: 13, image: "../Gallery/shutter.jpg", title: "Louvre Shutters", category: "Shutters", span: "col-span-2 row-span-2" },
+  { id: 14, image: "../Gallery/shutter2.jpg", title: "Motorized Shutters", category: "Shutters", span: "" },
+  { id: 15, image: "../Gallery/shutter3.jpg", title: "Vertical Blinds", category: "Shutters", span: "" },
+  { id: 16, image: "../Gallery/shutter4.jpg", title: "Roller Shutters", category: "Shutters", span: "" },
   
   // Balcony
   { id: 17, image: "../Gallery/balcony1.jpg", title: "Balcony Enclosure", category: "Balcony", span: "col-span-2 row-span-2" },
