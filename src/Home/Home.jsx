21fed0f97744df4c2b92028fd76a587684dee064
qtechx001 <qtechx001@gmail.com>
2026-07-07 10:46:59 +0530

    Home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Hero from './Hero'
 import HomeAbout from './HomeAbout'
 import HomeProducts from './HomeProducts'
@@ -13,8 +14,54 @@ import CompanyLogos from './CompanyLogos'
 import InstagramReels from './InstagramReels'
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 900);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
+    <motion.div
+      initial={{
+        opacity: 0,
+        scale: 0.8,
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        duration: 0.7,
+        ease: "easeOut",
+      }}
+    >
+      <AnimatePresence>
+        {loading && (
+          <>
+            {/* Left Curtain */}
+            <motion.div
+              initial={{ x: 0 }}
+              animate={{ x: "-100%" }}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 0.8 }}
+              className="fixed top-0 left-0 w-1/2 h-screen bg-[#0c5940]/30 z-[9999]"
+            />
+
+            {/* Right Curtain */}
+            <motion.div
+              initial={{ x: 0 }}
+              animate={{ x: "100%" }}
+              exit={{ x: "100%" }}
+              transition={{ duration: 0.8 }}
+              className="fixed top-0 right-0 w-1/2 h-screen bg-[#08124E]/30 z-[9999]"
+            />
+          </>
+        )}
+      </AnimatePresence>
       <Hero />
       <HomeAbout />
       {/* <OfferBanner /> */}
@@ -27,7 +74,7 @@ const Home = () => {
       <HomeFAQ />
       <HomeReviews />
       {/* <CompanyLogos /> */}
-    </div>
+    </motion.div>
   )
 }
 
